477fe15e3efc80e93c29e6882a2559e099b9e844
jabier espinal <jabierespinal@gmail.com>
2018-02-01 02:02:51 +0000

    finalised code part 3

--- lvl3/app.js
+++ lvl3/app.js
@@ -1,18 +1,44 @@
 var express = require('express');
 var app = express();
 var datetime = new Date();
+var cities = {
+  "Providence": "Rhode Island",
+  "Cranston": "Rhode Island",
+  "Cumberland": "Rhode Island",
+  "Newport": "Rhode Island",
+  "pawtucket": "Rhode Island",
+  "Boston": "Massachusetts",
+  "Hartford": "Connecticut",
+};
 
 app.use(express.static('public'));
 
-app.get('/cities', function(req, res){
-  var cities = ['Providence', 'Cranston', 'Cumberland', 'Newport', 'Pawtucket', 'Lincoln'];
-  res.send(cities);
+app.param('name', function(req, res, next) {
+   var name = req.params.name;
+   var city = name[0].toUpperCase() + name.slice(1).toLowerCase();
+   req.cityName = city;
+   next();
 });
 
+app.get('/cities', function(req, res){
+    if (req.query.limit >= 0) { 
+        res.json(cities.slice(0, req.query.limit));
+   } else if (req.query.limit > cities.length) {
+      res.status(404, "Error not enough Cities");
+   } else  {
+      res.json(Object.keys(cities));
+   }
+});
 
-app.get('/date', function(request, response) {
-  response.redirect(datetime)
-})
+app.get("/cities/:name", function(req, res) {
+   var description = cities[req.cityName];
+   if (!description) {
+      res.status(404).json("Error No description found");
+   } else {
+      res.json(description);
+   }
+});
+
 
 app.listen(process.env.PORT, function() {
   console.log('Listening on c9');
